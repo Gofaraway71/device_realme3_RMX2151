6d54850820e8d2b5e3c64447b4096a06ea46fdb7
SamarV-121 <samarvispute121@gmail.com>
2021-08-19 21:36:32 +0530

    RMX2151: Remove libshowlogo shim
    Signed-off-by: noobyysauraj <gitsauraj@gmail.com>

--- device.mk
+++ device.mk
@@ -129,10 +129,6 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
-# Symbols
-PRODUCT_PACKAGES += \
-    libshim_showlogo
-
 # Vendor overlay
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PLATFORM_VNDK_VERSION))
