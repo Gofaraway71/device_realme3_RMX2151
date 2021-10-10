617eb41fea846d3005e1edca161310b9054a57e0
noobyysauraj <gitsauraj@gmail.com>
2021-10-10 11:57:16 +0530

    RMX2151: Revert Enable MTP/ADB by default
    This reverts commit c5cefa25328bd917c71d61fca294217ff2f11246.

--- system_prop.mk
+++ system_prop.mk
@@ -108,11 +108,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.sdha_apps_bg_max=64 \
     ro.config.sdha_apps_bg_min=8
 
-# set default USB configuration
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    sys.usb.config=mtp,adb \
-    persist.sys.usb.config=mtp,adb \
-
 # WFD
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.wfd.nohdcp=1 \
